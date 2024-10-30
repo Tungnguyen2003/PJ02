9a8c5cb66405d810c5c4f96908c51c08d03347b9
Tungnguyen2003 <nguyenviettung272@gmail.com>
2024-10-30 16:21:30 +0700

    Update second

--- PJ02/resources/views/auth/resigter.blade.php
+++ PJ02/resources/views/auth/resigter.blade.php
@@ -1,1 +1,29 @@
+<<<<<<< HEAD
 Resigter aaaaaaaaa 
+=======
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Resigter Page</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="https//cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+</head>
+<body>
+    <div class="containet">
+        <div class="wrapper">
+            <div class="tittle"><span> Resigter Page </span></div>
+            <form>
+                    <div class="row">
+                        <i class=" fas far-user"></i>
+                        <input type ="text" valua="" placeholder="Username" required name="name">
+                    </div>
+            </form>
+        </div>
+    </div>
+
+</body>
+</html>
+>>>>>>> cd7b982 (Update second)
